40ec188bae7666967a370cab1dbfc0676741103c
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-07-30 17:43:35 +0300

    correction fron pycodestyle

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -22,6 +22,7 @@ def get_cities_by_state(state_id):
 
     return jsonify(list_cities)
 
+
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     """ list a specific city throuht it's id """
