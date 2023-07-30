3f1a0dc49bf971468f5653c13b0f05a11ae8ff24
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-07-30 13:33:53 +0300

    correction

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,19 @@
+#!/usr/bin/python3
+
 """ Create the Flask application and blueprint """
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
-import os
+from os import environ
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
